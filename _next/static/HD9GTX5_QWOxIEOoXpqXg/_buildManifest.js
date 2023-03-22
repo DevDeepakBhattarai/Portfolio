43ff25b63af46ab2edba03a3a0057409e661bf16
DevDeepakBhattarai <108481783+DevDeepakBhattarai@users.noreply.github.com>
2023-03-22 10:06:01 +0000

    Deploying to gh-pages from @ DevDeepakBhattarai/Portfolio@47a18fe31d78f4e216cea6050473308e3bc108a2 🚀

--- _next/static/HD9GTX5_QWOxIEOoXpqXg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/58a3678d-68e1a733d372a43c.js","static/chunks/438-204d4039d2c0f3cf.js","static/chunks/pages/index-a28e0156bbaeb372.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
